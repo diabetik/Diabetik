52eee1e37a527783b9336a455a734b90ad993758
Nial Giacomelli <nialg@pinkfroot.com>
2013-12-28 19:07:16 +0000

    Ditched some redundant code

--- Diabetik/View Controllers/Export/UAExportViewController.m
+++ Diabetik/View Controllers/Export/UAExportViewController.m
@@ -863,20 +863,10 @@
 #pragma mark - UAModalViewDelegate methods
 - (void)willDisplayModalView:(UAModalView *)aModal
 {
-    /*
-     // Disable swipe gesture
-     JASidePanelController *sidePanel = (JASidePanelController *)[self sidePanelController];
-     sidePanel.disablePanGesture = YES;
-     */
+    // STUB
 }
 - (void)didDismissModalView:(UAModalView *)aModal
 {
-    /*
-     // Re-enable swipe gesture
-     JASidePanelController *sidePanel = (JASidePanelController *)[self sidePanelController];
-     sidePanel.disablePanGesture = NO;
-     */
-    
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kHasSeenExportTooltip];
     [[NSUserDefaults standardUserDefaults] synchronize];
 }
