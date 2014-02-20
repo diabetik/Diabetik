cbf0f7bd962d9af1e205314f7c63cc877eabdbe9
Nial Giacomelli <nialg@pinkfroot.com>
2014-02-20 17:38:40 +0000

    Removed the Insulin Calculator

--- Diabetik/View Controllers/Menu/UASideMenuViewController.m
+++ Diabetik/View Controllers/Menu/UASideMenuViewController.m
@@ -106,7 +106,7 @@
 {
     if(section == 0)
     {
-        return 6;
+        return 5;
     }
     else if(section == 1)
     {
@@ -143,31 +143,33 @@
             cell.accessoryIcon.image = [UIImage imageNamed:@"ListMenuIconJournal"];
             cell.accessoryIcon.highlightedImage = [UIImage imageNamed:@"ListMenuIconJournalHighlighted"];
         }
+        /*
         else if(indexPath.row == 1)
         {
             cell.textLabel.text = NSLocalizedString(@"Insulin Calculator", nil);
             cell.accessoryIcon.image = [UIImage imageNamed:@"ListMenuIconCalculator"];
             cell.accessoryIcon.highlightedImage = [UIImage imageNamed:@"ListMenuIconCalculatorHighlighted"];
         }
-        else if(indexPath.row == 2)
+        */
+        else if(indexPath.row == 1)
         {
             cell.textLabel.text = NSLocalizedString(@"Tags", nil);
             cell.accessoryIcon.image = [UIImage imageNamed:@"ListMenuIconTags"];
             cell.accessoryIcon.highlightedImage = [UIImage imageNamed:@"ListMenuIconTagsHighlighted"];
         }
-        else if(indexPath.row == 3)
+        else if(indexPath.row == 2)
         {
             cell.textLabel.text = NSLocalizedString(@"Reminders", nil);
             cell.accessoryIcon.image = [UIImage imageNamed:@"ListMenuIconReminders"];
             cell.accessoryIcon.highlightedImage = [UIImage imageNamed:@"ListMenuIconRemindersHighlighted"];
         }
-        else if(indexPath.row == 4)
+        else if(indexPath.row == 3)
         {
             cell.textLabel.text = NSLocalizedString(@"Export", @"Menu item to take users to the export screen");
             cell.accessoryIcon.image = [UIImage imageNamed:@"ListMenuIconExport"];
             cell.accessoryIcon.highlightedImage = [UIImage imageNamed:@"ListMenuIconExportHighlighted"];
         }
-        else if(indexPath.row == 5)
+        else if(indexPath.row == 4)
         {
             cell.textLabel.text = NSLocalizedString(@"Settings", nil);
             cell.accessoryIcon.image = [UIImage imageNamed:@"ListMenuIconSettings"];
@@ -268,7 +270,7 @@
         {
             [navigationController popToRootViewControllerAnimated:animateViewControllerChange];
         }
-        
+        /*
         else if(indexPath.row == 1)
         {
             if(![[navigationController topViewController] isKindOfClass:[UAInsulinCalculatorViewController class]])
@@ -277,7 +279,8 @@
                 [navigationController pushViewController:vc animated:animateViewControllerChange];
             }
         }
-        else if(indexPath.row == 2)
+        */
+        else if(indexPath.row == 1)
         {
             if(![[navigationController topViewController] isKindOfClass:[UATagsViewController class]])
             {
@@ -285,7 +288,7 @@
                 [navigationController pushViewController:vc animated:animateViewControllerChange];
             }
         }
-        else if(indexPath.row == 3)
+        else if(indexPath.row == 2)
         {
             if(![[navigationController topViewController] isKindOfClass:[UARemindersViewController class]])
             {
@@ -293,7 +296,7 @@
                 [navigationController pushViewController:vc animated:animateViewControllerChange];
             }
         }
-        else if(indexPath.row == 4)
+        else if(indexPath.row == 3)
         {
             if(![[navigationController topViewController] isKindOfClass:[UAExportViewController class]])
             {
@@ -301,7 +304,7 @@
                 [navigationController pushViewController:vc animated:animateViewControllerChange];
             }
         }
-        else if(indexPath.row == 5)
+        else if(indexPath.row == 4)
         {
             if(![[navigationController topViewController] isKindOfClass:[UASettingsViewController class]])
             {
